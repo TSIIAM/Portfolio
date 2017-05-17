8b882e7e410c8a76078586d0e74954a2e45c5282
Alvaromesa <tsi.iamesa@gmail.com>
2017-05-17 16:34:56 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+var main = function() {
+  $('.nav li').click(function() {
+    var category = $(this).attr('class');
+
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+		$(".thumbnail").hide();
+    $(".thumbnail").addClass('flex');
+    if(category == "nav-ejercicios"){
+      $(".thumbnail ").removeClass("selected");
+      $(".ejercicios ").addClass("selected");
+      $(".ejercicios ").toggle();
+      
+
+
+    }else if(category == "nav-estaticos"){
+       $(".thumbnail ").removeClass("selected");
+      $(".estaticos ").addClass("selected");
+      $(".estaticos ").toggle();
+
+
+    }else if(category == "nav-adaptativos"){
+       $(".thumbnail ").removeClass("selected");
+      $(".adaptativos ").addClass("selected");
+      $(".adaptativos ").toggle();
+
+
+    }else if(category == "nav-dinamicos"){
+       $(".thumbnail ").removeClass("selected");
+      $(".dinamicos ").addClass("selected");
+     $(".dinamicos ").toggle();
+
+
+
+    }else if(category == "nav-all"){
+      $(".thumbnail ").removeClass("selected");
+      $(".thumbnail").toggle();
+
+
+    }
+
+
+
+  });
+};
+
+$(document).ready(main);
